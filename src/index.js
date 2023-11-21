799e129c8aa0732bb19d13ffe11e66d12d52f70a
haegurii <yoohaedong3@gmail.com>
2023-11-21 12:00:06 +0900

    mergeWebRTC

--- src/index.js
+++ src/index.js
@@ -157,7 +157,6 @@ const createNewRoomHandler = (data, socket) => {
   // emit an event to all users connected
   // to that room about new users which are right in this room
   socket.emit("room-update", { connectedUsers: newRoom.connectedUsers });
-  
 };
 
 const joinRoomHandler = (data, socket) => {
--- src/routes/signLanguages.js
+++ src/routes/signLanguages.js
@@ -2,31 +2,55 @@ const express = require("express");
 const SignLanguage = require("../models/SignLanguage");
 const router = express.Router();
 
+router.get("/consonant/index", async (req, res, next) => {
+  try {
+    const index = await SignLanguage.find({ type: "consonant" }).count();
+    return res.status(200).json({ index });
+  } catch (error) {
+    next(error);
+  }
+});
 router.get("/consonant", async (req, res, next) => {
   try {
-    const signLanguages = await SignLanguage.find({ type: "consonant" });
-    // return res.status(200).json({
-    // title: products.title,
-    // description: req.signlanguage.description,
-    // type: req.signlanguage.type,
-    // images: req.signlanguage.images,
-    // video: req.signlanguage.video,
-    // });
+    const page = req.query.page ? Number(req.query.page) : 1;
+    const limit = req.query.limit ? Number(req.query.limit) : 5;
+
+    const skip = (page - 1) * limit;
+
+    const signLanguages = await SignLanguage.find({ type: "consonant" })
+      .sort([["name", "asc"]])
+      .skip(skip)
+      .limit(limit);
+
     return res.status(200).json({ signLanguages });
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/", async (req, res, next) => {
+router.get("/search", async (req, res, next) => {
   try {
-    const SignLanguages = new SignLanguage(req.body);
+    // const page = req.query.page ? Number(req.query.page) : 1;
+    // const limit = req.query.limit ? Number(req.query.limit) : 10;
 
-    await SignLanguages.save();
-    return res.sendStatus(200);
+    // const skip = (page - 1) * limit;
+    const search = req.query.search ? String(req.query.search) : "";
+    const signLanguages = await SignLanguage.find({ name: search });
+
+    return res.status(200).json({ signLanguages });
   } catch (error) {
     next(error);
   }
 });
+// router.post("/", async (req, res, next) => {
+//   try {
+//     const SignLanguages = new SignLanguage(req.body);
+
+//     await SignLanguages.save();
+//     return res.sendStatus(200);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 module.exports = router;
