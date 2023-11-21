7618d5c9841d9194621171ccb273d6e393218b45
haegurii <yoohaedong3@gmail.com>
2023-11-21 17:05:41 +0900

    wordLearn

--- src/routes/signLanguages.js
+++ src/routes/signLanguages.js
@@ -16,7 +16,6 @@ router.get("/consonant", async (req, res, next) => {
     const limit = req.query.limit ? Number(req.query.limit) : 5;
 
     const skip = (page - 1) * limit;
-
     const signLanguages = await SignLanguage.find({ type: "consonant" })
       .sort([["name", "asc"]])
       .skip(skip)
@@ -27,7 +26,33 @@ router.get("/consonant", async (req, res, next) => {
     next(error);
   }
 });
+router.get("/word/index", async (req, res, next) => {
+  try {
+    const index = await SignLanguage.find({ type: "word" }).count();
+    return res.status(200).json({ index });
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/word", async (req, res, next) => {
+  try {
+    const page = req.query.page ? Number(req.query.page) : 1;
+    const limit = req.query.limit ? Number(req.query.limit) : 5;
 
+    const skip = (page - 1) * limit;
+    const signLanguages = await SignLanguage.find({
+      type: "word",
+      name: { $ne: "#NAME?" },
+    })
+      .sort([["name", "asc"]])
+      .skip(skip)
+      .limit(limit);
+
+    return res.status(200).json({ signLanguages });
+  } catch (error) {
+    next(error);
+  }
+});
 router.get("/search", async (req, res, next) => {
   try {
     // const page = req.query.page ? Number(req.query.page) : 1;
