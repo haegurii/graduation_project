988ffc79d548f1e18186f3b9982f59c9c7505f56
haegurii <yoohaedong3@gmail.com>
2023-11-27 22:17:06 +0900

    문자열 포함으로 검색

--- src/routes/signLanguages.js
+++ src/routes/signLanguages.js
@@ -54,27 +54,15 @@ router.get("/word", async (req, res, next) => {
 });
 router.get("/search", async (req, res, next) => {
   try {
-    // const page = req.query.page ? Number(req.query.page) : 1;
-    // const limit = req.query.limit ? Number(req.query.limit) : 10;
-
-    // const skip = (page - 1) * limit;
     const search = req.query.search ? String(req.query.search) : "";
-    const signLanguages = await SignLanguage.find({ name: search });
+    const signLanguages = await SignLanguage.find({
+      name: { $regex: search, $options: "i" },
+    });
 
     return res.status(200).json({ signLanguages });
   } catch (error) {
     next(error);
   }
 });
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const SignLanguages = new SignLanguage(req.body);
-
-//     await SignLanguages.save();
-//     return res.sendStatus(200);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 module.exports = router;
